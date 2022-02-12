149e7902a8a36939a7b1997c99c840058f8422fc
Bruno Nascimento <bruno@brunonascimento.com>
2022-02-12 18:43:53 -0300

    Funcao js para gerar o codigo

--- js/main.js
+++ js/main.js
@@ -1,32 +1,28 @@
 
  
-//https://api.whatsapp.com/send?phone=5521974561726&text=$mgs_whats
- 
+
+
+/*
+ https://api.whatsapp.com/send?phone=5521974561726&text=$mgs_whats
+})*/
+
 function capturar(e) {
     e.preventDefault();
-    capturando = document.getElementById('telefone').value;
+        telefone = document.getElementById('telefone').value;
  
     msg = document.getElementById('exampleFormControlTextarea1').value;
 
     document.getElementById('link-what').style="display:block";
 
-    
+    msg_whats = msg.replace(/ /g, "%20");
 
     var NewLink = "https://api.whatsapp.com/send?phone=";
-    var linkGerado = NewLink + capturando + msg ;
-    console.log(capturando + msg + NewLink);
+    var linkGerado = NewLink + "55"+ telefone + "&text=" + msg_whats ;
+    console.log(telefone + msg + NewLink);
+     
+
     document.getElementById('valorDigitado').innerHTML = linkGerado;
 
 
 }
 
-/*
-form.addEventListener('submit', event => {
-    event.preventDefault()
-      
-    const telefoneDig = telefone.value
-    console.log(telefoneDig)
-      
-
-      
-})*/

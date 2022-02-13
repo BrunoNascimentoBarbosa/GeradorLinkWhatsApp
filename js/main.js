e18788ab4f09efe5e966f01a00ac2e11339219a8
Bruno Nascimento <bruno@brunonascimento.com>
2022-02-12 22:03:49 -0300

    Criada a funcao para copiar o link gerado

--- js/main.js
+++ js/main.js
@@ -1,28 +1,26 @@
 
- 
-
-
-/*
- https://api.whatsapp.com/send?phone=5521974561726&text=$mgs_whats
-})*/
-
 function capturar(e) {
     e.preventDefault();
-        telefone = document.getElementById('telefone').value;
- 
+    telefone = document.getElementById('telefone').value;
     msg = document.getElementById('exampleFormControlTextarea1').value;
-
     document.getElementById('link-what').style="display:block";
-
     msg_whats = msg.replace(/ /g, "%20");
-
     var NewLink = "https://api.whatsapp.com/send?phone=";
     var linkGerado = NewLink + "55"+ telefone + "&text=" + msg_whats ;
+
+
     console.log(telefone + msg + NewLink);
-     
-
-    document.getElementById('valorDigitado').innerHTML = linkGerado;
-
+  
+    document.getElementById('valorDigitado').value = linkGerado;
+   //document.getElementById('valorDigitado').innerHTML = linkGerado;
 
 }
 
+function copiarLink() {
+    let copiar = document.getElementById("valorDigitado");
+ 
+    copiar.select();
+    copiar.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(copiar.value);
+     
+ }

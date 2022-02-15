3b7db6227d85e62d0252818f3ac38bec5bb860ed
Bruno Nascimento <bruno@brunonascimento.com>
2022-02-14 22:38:36 -0300

    Ajuste na funcao copiar o link gerado e melhorais do font-end

--- js/main.js
+++ js/main.js
@@ -4,21 +4,24 @@ function capturar(e) {
     telefone = document.getElementById('telefone').value;
     msg = document.getElementById('exampleFormControlTextarea1').value;
     document.getElementById('link-what').style="display:block";
+    
     msg_whats = msg.replace(/ /g, "%20");
     var NewLink = "https://api.whatsapp.com/send?phone=";
     var linkGerado = NewLink + "55"+ telefone + "&text=" + msg_whats ;
-
+    
 
     console.log(telefone + msg + NewLink);
   
     document.getElementById('valorDigitado').value = linkGerado;
+
+    document.getElementById('form_01').style="display:none";
+     document.getElementById('bnt').style="display:none";
    //document.getElementById('valorDigitado').innerHTML = linkGerado;
 
 }
 
 function copiarLink() {
     let copiar = document.getElementById("valorDigitado");
- 
     copiar.select();
     copiar.setSelectionRange(0, 99999);
     navigator.clipboard.writeText(copiar.value);
